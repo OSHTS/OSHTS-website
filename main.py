bab3e8a48f61c64a359e3f331ffc202d6c5ffda2
OSHTS <adunicholas74@gmail.com>
2020-02-05 23:42:30 -0800

    updates

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:////database.db'
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
-    return render_template("main.html")
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
