b2128972053db3b8217ee31927c81d324fb4ef9e
you@example.com <you@example.com>
2020-02-16 10:25:19 -0800

    added login and sign up pages

--- main.py
+++ main.py
@@ -18,5 +18,13 @@ def about():
 def courses():
     return render_template("forume.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
