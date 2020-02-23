b9bc31bea3ab0e22ede2cd891dfcd8d3e986338a
OSHTS <adunicholas74@gmail.com>
2020-02-22 16:56:47 -0800

    chatroom

--- static/js/chatroom.js
+++ static/js/chatroom.js
@@ -7,3 +7,16 @@ function send(){
   }
 
 }
+
+
+fetch('http://127.0.0.1:5000/messages')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+  })
