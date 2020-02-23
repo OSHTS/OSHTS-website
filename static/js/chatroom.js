e928c5b099afc3df90ec7963ec51b3eeb7a9d943
OSHTS <adunicholas74@gmail.com>
2020-02-23 14:13:47 -0800

    error update

--- static/js/chatroom.js
+++ static/js/chatroom.js
@@ -5,7 +5,7 @@ function send(){
   var message = document.getElementById("message");
   var screen1 = document.getElementById("screen");
   if(message.value != ""){
-      screen1.innerHTML += "<div class='message txt2'>" + message.value + "</div>"
+      screen1.innerHTML += "<div class='message txt2 sent_msg'>" + message.value + "</div>"
       fetch(onlinehost + 'post_msg/' + message.value)
       message.value = ""
   }
@@ -23,7 +23,7 @@ function getMsg(){
       var screen1 = document.getElementById("screen")
       // Work with JSON data here
       for(var x = 0; x <= data.length - 1; x++){
-        screen1.innerHTML += "<div class='message txt2 sent_msg'>" + data[x].message + "</div>"
+        screen1.innerHTML += "<div class='message txt2'>" + data[x].message + "</div>"
         fetch(onlinehost + 'send_msg/' + data[x].id)
       }
     })
